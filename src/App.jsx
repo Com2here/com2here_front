0bcd60f672ca38309b325c9db6c66dea610be434
hhjkhhbgkugjnbj <ck8901ck@gmail.com>
2025-04-01 02:57:51 +0900

    Refector : merge error fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import SupportPage from "./pages/SupportPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import MyPage from "./pages/MyPage";
+
+import MylistPage from "./pages/MylistPage";
+import AccountPage from "./pages/AccountPage";
+import EstimatePage from "./pages/EstimatePage";
+import ResetPassword from "./components/ResetPassword";
+import PcComparisonPage from "./pages/PcComparisonPage";
+
 import FindPasswordPage from "./pages/FindPasswordPage";
 import { AuthProvider } from "./contexts/AuthContext";
 import KakaoCallback from "./components/KakaoCallback";
@@ -15,21 +22,32 @@ import GoogleCallback from "./components/GoogleCallback.jsx";
 
 function App() {
   return (
-    <AuthProvider>
-      <Header></Header>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/support" element={<SupportPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/find-password" element={<FindPasswordPage />} />
-        <Route path="/callback/kakao" element={<KakaoCallback />} />
-        <Route path="/callback/naver" element={<NaverCallback />} />
-        <Route path="/callback/google" element={<GoogleCallback />} />
-      </Routes>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <Router>
+          <Header></Header>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/support" element={<SupportPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/mylist" element={<MylistPage />}></Route>
+            <Route path="/account" element={<AccountPage />}></Route>
+            <Route path="/estimate" element={<EstimatePage />}></Route>
+            <Route path="/reset-password" element={<ResetPassword />}></Route>
+            <Route path="/find-password" element={<FindPasswordPage />} />
+            <Route path="/callback/kakao" element={<KakaoCallback />} />
+            <Route path="/callback/naver" element={<NaverCallback />} />
+            <Route path="/callback/google" element={<GoogleCallback />} />
+            <Route
+              path="/pc-comparison"
+              element={<PcComparisonPage />}
+            ></Route>
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </HelmetProvider>
   );
 }
 
