cb2ebb894b290de148dec7ca3e4b890c852a88ad
sy <ssyeo_n@naver.com>
2025-04-05 11:00:14 +0900

    ♻️ refactor: Router 컴포넌트 분리
    - App.jsx에서 라우팅 관련 코드를 Router.jsx로 분리하여 가독성 향상

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,13 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { AuthProvider } from "./contexts/AuthContext";
+import { Router } from "./routes/Router";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
-import SupportPage from "./pages/SupportPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import MyPage from "./pages/MyPage";
-import MylistPage from "./pages/MylistPage";
-import AccountPage from "./pages/AccountPage";
-import EstimatePage from "./pages/EstimatePage";
-import PcComparisonPage from "./pages/PcComparisonPage";
-import FindPwPage from "./pages/FindPwPage";
-
-import OAuthCallback from "./oauth/OAuthCallback.jsx";
-import AccountDeletePage from "./pages/AccountDeletePage.jsx";
-import ScrollToTop from "./utils/scrollToTop.js";
 function App() {
   return (
     <HelmetProvider>
       <AuthProvider>
-          <Router>
-          <ScrollToTop />
-            <Header></Header>
-            <div className="main-content">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/support" element={<SupportPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/mypage" element={<MyPage />} />
-                <Route path="/mylist" element={<MylistPage />}></Route>
-                <Route path="/account" element={<AccountPage />}></Route>
-                <Route
-                  path="/account/delete"
-                  element={<AccountDeletePage />}
-                ></Route>
-                <Route path="/estimate" element={<EstimatePage />}></Route>
-                <Route
-                  path="/pc-comparison"
-                  element={<PcComparisonPage />}
-                ></Route>
-                <Route path="/help/findPw" element={<FindPwPage />}></Route>
-
-                <Route
-                  path="/callback/kakao"
-                  element={<OAuthCallback provider="kakao" />}
-                />
-                <Route
-                  path="/callback/naver"
-                  element={<OAuthCallback provider="naver" />}
-                />
-                <Route
-                  path="/callback/google"
-                  element={<OAuthCallback provider="google" />}
-                />
-              </Routes>
-            </div>
-            <Footer></Footer>
-          </Router>
+        <Router />
       </AuthProvider>
     </HelmetProvider>
   );
