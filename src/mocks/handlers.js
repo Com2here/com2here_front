6bea4162a2fb8097ccdaf86aa4d4df89b0581ced
sy <ssyeo_n@naver.com>
2025-04-22 05:38:41 +0900

    ✨ feat/#46: 회원가입 - 에러 핸들링
    - errors 상수 파일에 회원가입 에러 메시지 추가
    - 회원가입 실패 시 에러 코드에 따른 메시지 출력
    - RegisterForm에서 api 모듈 사용으로 변경
    - CSS 스타일 수정: Footer의 최대 너비 및 RegisterPage 스타일 정리

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,27 +1,17 @@
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.post("/api/v1/email/password/reset", async ({ request }) => {
-    const { mail, code, password, confirmPassword } = await request.json();
+  http.post("/api/v1/user/register", async ({ request }) => {
+    const { nickname, email, password, confirmPassword } = await request.json();
 
-    if (
-      mail === "test@example.com" &&
-      code === "123456" &&
-      password === "pw12345!" &&
-      confirmPassword === "pw12345!"
-    ) {
+    // 이미 가입된 이메일 테스트 케이스
+    if (email === "test1@example.com") {
       return HttpResponse.json(
         {
-          code: 501,
-          message: "비밀번호가 성공적으로 변경되었습니다.",
+          code: 500,
         },
         { status: 200 },
       );
     }
-
-    return HttpResponse.json(
-      { message: "이메일 또는 비밀번호가 올바르지 않습니다." },
-      { status: 401 },
-    );
   }),
 ];
