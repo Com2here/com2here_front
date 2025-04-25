f625a1a47e8985e6c7da3ecfa421f37579609fcd
sy <ssyeo_n@naver.com>
2025-04-26 03:18:43 +0900

    ➕ chore: ESLint 설정 업데이트 - import 정렬 자동화
    - ESLint 설정 파일에서 simple-import-sort 플러그인 추가
    - ESLint 규칙에 simple-import-sort 관련 규칙 추가
    - package.json 및 package-lock.json에 eslint-plugin-simple-import-sort 의존성 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,26 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact, { rules } from "eslint-plugin-react";
+import pluginReact from "eslint-plugin-react";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
     rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
       "react/react-in-jsx-scope": "off",
     },
   },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
