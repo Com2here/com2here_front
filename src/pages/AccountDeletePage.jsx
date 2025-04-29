8de20763fa8382cd365117d60c177fd8aa5ed904
sy <ssyeo_n@naver.com>
2025-04-29 21:16:36 +0900

    ✨ feat: 계정 삭제 페이지에 비밀번호 보기/숨기기 기능 추가 및 스타일 수정

--- src/pages/AccountDeletePage.jsx
+++ src/pages/AccountDeletePage.jsx
@@ -1,12 +1,20 @@
+import "../styles/AccountDeletePage.css";
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 import { useAuth } from "../contexts/AuthContext";
-import "../styles/AccountDeletePage.css";
 import api from "../hooks/useAxios";
 
 const AccountDeletePage = () => {
+  const imgPathEye = "/images/eye.svg";
+  const imgPathEyeSlash = "/images/eye-slash.svg";
+
   const navigate = useNavigate();
+
   const [password, setPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
   const { logout } = useAuth();
 
   const handleDelete = async () => {
@@ -40,16 +48,30 @@ const AccountDeletePage = () => {
     }
   };
 
+  const togglePassword = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
   return (
     <div className="account-delete-container">
       <h2>계정 삭제</h2>
       <p>계정을 삭제하면 복구할 수 없습니다. 정말 삭제하시겠습니까?</p>
-      <input
-        type="password"
-        placeholder="비밀번호 입력"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      <div className="account-delete-input-password">
+        <input
+          type={isPasswordVisible ? "text" : "password"}
+          placeholder="비밀번호 입력"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="account-delete-pw-right">
+              <button type="button" onClick={togglePassword}>
+                <img
+                  src={isPasswordVisible ? imgPathEyeSlash : imgPathEye}
+                  alt="비밀번호 보기"
+                />
+              </button>
+            </div>
+      </div>
       <div className="button-group">
         <button className="delete-button" onClick={handleDelete}>
           삭제하기
